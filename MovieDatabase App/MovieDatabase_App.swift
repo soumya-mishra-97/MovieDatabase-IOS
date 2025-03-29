bf978175f2eb9aeae21b5c811d6e1fc9bf7add92
soumya-mishra-97 <soumyamishra637@gmail.com>
2025-03-30 02:37:46 +0530

    Setup

---
+++ MovieDatabase App/MovieDatabase_App.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDatabase_App.swift
+//  MovieDatabase App
+//
+//  Created by Soumya Mishra on 29/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieDatabase_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
